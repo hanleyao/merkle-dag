231abb20b716b81efc18dd7b492bce58515faf4a
hanleyao <2924039591@qq.com>
2024-03-05 20:17:34 +0800

    new

--- dag.go
+++ dag.go
@@ -2,6 +2,17 @@ package merkledag
 
 import "hash"
 
+type Link struct {
+	Name string
+	Hash []byte
+	Size int
+}
+
+type Object struct {
+	Links []Link
+	Data  []byte
+}
+
 func Add(store KVStore, node Node, h hash.Hash) []byte {
 	// TODO 将分片写入到KVStore中，并返回Merkle Root
 	return nil
